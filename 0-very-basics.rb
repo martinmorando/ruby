e609210aa43e16b651abc64a56efcbaaa25ea530
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-12 20:46:08 -0300

    add very basics and strings

---
+++ 0-very-basics.rb
@@ -0,0 +1,15 @@
+=begin
+    This is a multi-line comment
+=end
+
+# Declare and assign variables
+nCoins = 21000000            # Integer
+blockReward = 3.125          # Float
+name = "Satoshi Nakamoto"    # String
+isRevolution = true          # Boolean
+
+# Output (no breakline)
+print nCoins
+
+# Output with break line
+puts name
---
+++ 1-strings.rb
@@ -0,0 +1,17 @@
+=begin
+    Strings
+=end
+
+name = "Satoshi Nakamoto"
+
+# Length
+puts name.length
+
+# Uppercase
+puts name.upcase
+
+# Lowercase
+puts name.downcase
+
+# Reverses the string
+puts name.reverse
