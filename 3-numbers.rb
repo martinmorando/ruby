5fcf73352952d1629affc8b56962cd10c98abc72
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-15 11:03:05 -0300

    add to_i method

--- 3-numbers.rb
+++ 3-numbers.rb
@@ -2,11 +2,12 @@
     Numbers	
 =end
 
-# Declare and assign variables
+# [Declare and assign variables]
 a = 123
 b = 5
 
-# Basic operations
+
+# [Basic operations]
 x1 = a + b
 x2 = a - b
 x3 = a * b
@@ -14,6 +15,17 @@ x4 = a / b
 x5 = a % b
 x6 = a ** 2
 
-# Absolute value
+
+# [Absolute value]
 c = -12
 absolute = c.abs
+
+
+# [Convert to integer]
+# - On strings
+string = "2"
+string.to_i     # 2
+
+# - On floats: truncates
+float = 1.23
+puts float.to_i # 1
