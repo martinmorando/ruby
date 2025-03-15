70dc430bdcc70693e15d959ea5d349b1cf91fb9c
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-15 11:03:45 -0300

    explain chomp method when accepting user input

--- n-input.rb
+++ n-input.rb
@@ -1,7 +1,9 @@
 =begin
     Input
 =end
-# Accept user input
+
+# [Accept user input]
+# - method .chomp is used to remove the newline character (\n) at the end
 print "Type in your name: "
 name = gets.chomp
 puts name
