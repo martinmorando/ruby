63872ddbf5478561be96dcedd27d03547ac0dc48
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-16 18:38:07 -0300

    add examples of for loop, until loop, and loop do to 5-loops.rb

--- 5-loops.rb
+++ 5-loops.rb
@@ -1,10 +1,42 @@
 =begin
-    Loops 
+    Loops
 =end
 
+# [For loop]
+# - Alternative A: including range
+for i in 1..10
+    puts "for loop (A): #{i}"
+end
+
+# - Alternative B: excluding range
+for i in 1...11
+    puts "for loop (B): #{i}"
+end
+
+
+
 # [While loop]
-i = 0
-while i < 10
-    puts i
-    i = i + 1  # Post-increment operator (i++) does not exist in Ruby
+i = 1
+while i <= 10
+    puts "while loop: #{i}"
+    i += 1    # Post-increment operator (i++) does not exist in Ruby
+end
+
+
+
+# [Until loop]
+i = 1
+until i == 11
+    puts "until loop: #{i}"
+    i += 1
+end
+
+
+
+# [Loop do]
+i = 1
+loop do    
+    puts "loop do: #{i}"
+    i += 1
+    break if i == 11
 end
