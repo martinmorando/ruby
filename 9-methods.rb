77b8bba6cae3eae4ab3eb2e8926e51c1962edc25
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-20 15:25:01 -0300

    add default parameter in 9-methods.rb

--- 9-methods.rb
+++ 9-methods.rb
@@ -2,16 +2,18 @@
     Methods
 =end
 
-# Define a method with 2 parameters
-def sumExample(a, b)
+# Define a method with 2 parameters, one default
+def sumExample(a, b=8)
     return a + b
 end
 
 puts sumExample(2, 3)   # 5
+puts sumExample(2)      # 10
 
 
 # Splat arguments: the method can receive 1 or more arguments
 def splatExample(*b)
+    # Iterate over each item in the array 
     b.each do |item|
         puts "Item: #{item}"
     end
