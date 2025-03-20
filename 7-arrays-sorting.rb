8d34046c2e2cac656c862a2c3489aa7b8b1028bc
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-20 15:26:35 -0300

    add example for reversing arrays and reduce array sizes

--- 7-arrays-sorting.rb
+++ 7-arrays-sorting.rb
@@ -2,18 +2,24 @@
     Sorting arrays
 =end
 
-numbersA = [2, 6, 1, 3, 9, 4, 5]
-numbersB = [3, 5, 2, 2, 0, 5, 4]
+numbersA = [2, 6, 1, 3, 9]
+numbersB = [3, 5, 6, 2, 0]
 
 # [Ascending order]
 # - Alternative A
-print numbersA.sort!        # [1, 2, 3, 4, 5, 6, 9]
+print numbersA.sort!        # [1, 2, 3, 6, 9]
 
 # - Alternative B
-numbersB.sort! { |a, b| a <=> b }
-print numbersB              # [0, 2, 2, 3, 4, 5, 5]
+numbersA.sort! { |a, b| a <=> b }
+print numbersA              # [1, 2, 3, 6, 9]
+
+
+
+# [Reverse]
+print numbersB.reverse!     # [0, 2, 6, 5, 3]
+
 
 
 # [Descending order]
 numbersB.sort! { |a, b| b <=> a }
-print numbersB              # [5, 5, 4, 3, 2, 2, 0]
+print numbersB               # [6, 5, 3, 2, 0]
