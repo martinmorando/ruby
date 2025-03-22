61a550766db7cdb9367d04bc5a0c44dfd67fd1fd
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-22 09:50:50 -0300

    add .select method and hash iteration using only keys or values in 8-hashes.rb

--- 8-hashes.rb
+++ 8-hashes.rb
@@ -3,14 +3,14 @@
     - Collection of key-value pairs, with unique keys
 =end
 
-# [Create]
+# [CREATE]
 # - Create an empty hash
 a = Hash.new()
 
-# - Create an empty hash with default value of 0
+# - Create an empty hash with default value of 0 ("hash constructor notation")
 b = Hash.new(0)
 
-# - Create a hash with initial key-value pairs
+# - Create a hash with initial key-value pairs ("hash literal notation")
 c = {
     "A" => 123,
     "B" => 342,
@@ -18,21 +18,37 @@ c = {
 }
 
 
-# [Add key:value]
+# [ADD KEY:VALUE]
 a["Satoshi"] = "Nakamoto"
 
 
-# [Print to console]
+# [PRINT]
 puts c                          # {"A"=>123, "B"=>342, "E"=>212}
 
 
-# [Iterate]
+# [ITERATE]
+# - Iterate over key:value pairs
 c.each do |key,value|           # key, value are placeholders
     puts key + " " + value.to_s # A 123 \n B 324 \n E 212
 end
 
+# - Iterate over keys
+c.each_key do |key|
+    puts key
+end
 
-# [Sort]
+# - Iterate over values
+c.each_value do |value|
+    puts value
+end
+
+
+# [SELECT]
+c_filtered = c.select{ |key, value| value > 250 }   
+puts c_filtered                 # {"B"=>342}
+
+
+# [SORT]
 # - Sort ascending by values. "sort_by" returns a new array, doesn't modify original hash
 d = c.sort_by do |key,value|
     value
